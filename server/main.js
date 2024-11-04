2ad756bdf66ad9189874d779b8ffe96f8ba34ef3
Zeus <ravihusa@gmail.com>
2024-11-04 16:38:37 -0500

    connected to the database and made the basic login, and removed useless dependencies

--- server/main.js
+++ server/main.js
@@ -1,7 +1,80 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var mysql = require("mysql");
 var fs = require("node:fs");
 var http = require("node:http");
+//https://github.com/Zeuszzzzz/learningWebdev
+var PORT = 80;
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Ravi16nP14",
+    database: "credentials"
+});
+connection.connect(function (err) {
+    if (err)
+        throw err;
+});
+function loginCheck(Email, Password) {
+    return __awaiter(this, void 0, void 0, function () {
+        var err_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    return [4 /*yield*/, new Promise(function (resolve, reject) {
+                            connection.query("SELECT Email FROM credentials.new_table WHERE Email = \"".concat(Email, "\" AND Password = \"").concat(Password, "\""), function (err, result) {
+                                if (err)
+                                    return reject(err);
+                                resolve(result.length > 0);
+                            });
+                        })];
+                case 1: return [2 /*return*/, _a.sent()];
+                case 2:
+                    err_1 = _a.sent();
+                    console.error(err_1);
+                    return [2 /*return*/, false];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
+}
 http.createServer(function (req, res) {
     console.log("\n        =============================\n\n        ".concat(req.method, "\n\n         ").concat(req.url, "\n\n         ").concat(req.headers, "\n\n        =============================\n\n        "));
     switch (req.method) {
@@ -16,8 +89,8 @@ http.createServer(function (req, res) {
             res.end("405\nUnallowed http method");
             break;
     }
-}).listen(80, function () {
-    console.log("http://127.0.0.1:80");
+}).listen(PORT, function () {
+    console.log("http://127.0.0.1:" + PORT);
 });
 function get(requestUrl, res) {
     switch (requestUrl) {
@@ -73,23 +146,42 @@ function get(requestUrl, res) {
     }
 }
 function post(req, res) {
+    var _this = this;
     switch (req.url) {
         case "/login":
             var body_1 = "";
             req.on("data", function (chunk) {
                 body_1 = chunk.toString();
             });
-            req.on("end", function () {
-                var jsonBody = JSON.parse(body_1);
-                console.log(jsonBody);
-                try {
-                    res.writeHead(401, { "Content-type": "text/plain" });
-                    res.end("login success");
-                }
-                catch (err) {
-                    res.end("there was an error");
-                }
-            });
+            req.on("end", function () { return __awaiter(_this, void 0, void 0, function () {
+                var jsonBody, err_2;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            jsonBody = JSON.parse(body_1);
+                            console.log(jsonBody);
+                            _a.label = 1;
+                        case 1:
+                            _a.trys.push([1, 3, , 4]);
+                            return [4 /*yield*/, loginCheck(jsonBody.email, jsonBody.password)];
+                        case 2:
+                            if (_a.sent()) {
+                                res.writeHead(200, { "Content-type": "text/plain" });
+                                res.end("login success");
+                            }
+                            else {
+                                res.writeHead(401, { "Content-type": "text/plain" });
+                                res.end("login fail");
+                            }
+                            return [3 /*break*/, 4];
+                        case 3:
+                            err_2 = _a.sent();
+                            res.end("there was an error");
+                            return [3 /*break*/, 4];
+                        case 4: return [2 /*return*/];
+                    }
+                });
+            }); });
             req.on("error", function () {
                 res.writeHead(500, { "Content-Type": "text/plain" });
                 res.end("500 error\nSomething went Wrong");
@@ -101,3 +193,12 @@ function post(req, res) {
             break;
     }
 }
+process.on("SIGINT", function () {
+    console.log("Shutting down gracefully...");
+    connection.end(function (err) {
+        if (err) {
+            console.error("Error closing the connection: ", err);
+        }
+        process.exit();
+    });
+});
--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,38 @@
+import * as mysql from "mysql";
+import Connection from "mysql/lib/Connection";
 import * as fs from "node:fs";
 import * as http from "node:http";
+
+//https://github.com/Zeuszzzzz/learningWebdev
+const PORT : number = 80;
+const connection:Connection = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "Ravi16nP14",
+    database : "credentials"
+});
+connection.connect((err)=>{
+    if (err) throw err;
+});
+
+
+async function loginCheck(Email: string, Password: string): Promise<boolean> {
+    try {
+        return await new Promise<boolean>((resolve, reject) => {
+            connection.query(`SELECT Email FROM credentials.new_table WHERE Email = "${Email}" AND Password = "${Password}"`, (err, result) => {
+                if (err) return reject(err);
+                resolve(result.length > 0);
+            });
+        });;
+    } catch (err) {
+        console.error(err);
+        return false;
+    }
+}
+
+
+
+
 
 http.createServer((req:http.IncomingMessage, res:http.ServerResponse<http.IncomingMessage>) => {
     console.log(`
@@ -21,8 +54,8 @@ http.createServer((req:http.IncomingMessage, res:http.ServerResponse<http.Incomi
             res.end("405\nUnallowed http method");
             break;
     }
-}).listen(80,() =>{
-    console.log("http://127.0.0.1:80");
+}).listen(PORT,() =>{
+    console.log("http://127.0.0.1:" + PORT);
 });
 
 function get(requestUrl :string | undefined, res :http.ServerResponse<http.IncomingMessage>) : void{
@@ -87,14 +120,21 @@ function post(req:http.IncomingMessage,res:http.ServerResponse<http.IncomingMess
             req.on("data", (chunk:Buffer) => {
                 body = chunk.toString();
             })
-            req.on("end", ()=>{
+            req.on("end", async ()=>{
                 let jsonBody : {"email" :string, "password" : string} = JSON.parse(body);
                 console.log(jsonBody);
                 
                 
                 try{
-                    res.writeHead(401, {"Content-type" : "text/plain"});
-                    res.end("login success");
+                    if(await loginCheck(jsonBody.email, jsonBody.password)){
+                        res.writeHead(200, {"Content-type" : "text/plain"});
+                        res.end("login success");
+                    }
+                    else{
+                        res.writeHead(401, {"Content-type" : "text/plain"});
+                        res.end("login fail");
+                    }
+                    
                 }
                 catch(err){
                     res.end("there was an error");
@@ -112,3 +152,13 @@ function post(req:http.IncomingMessage,res:http.ServerResponse<http.IncomingMess
             break;
     }
 }
+process.on("SIGINT", ()=>{
+    console.log("Shutting down gracefully...");
+    connection.end((err) => {
+        if (err) {
+            console.error("Error closing the connection: ", err);
+        }
+        process.exit();
+    });
+
+});
